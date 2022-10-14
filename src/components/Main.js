4016b9eac06b12d4e70c2a02227c40693143f9f4
vmityagin <mityaginvs@yandex.ru>
2022-10-14 23:37:41 +0300

    navtab добавлена навигация с мобильной версткой

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Header from './Header';
 import Promo from './Main/Promo';
+import NavTab from './Main/NavTab';
 
 function Main() {
     return (
       <main className="page">
         <Header link="/signin" />
         <Promo />
+        <NavTab />
       </main>
     );
   }
-  
+
   export default Main;
