55ff1f5590ba111afc7fb446b0a4b0b201630862
vmityagin <mityaginvs@yandex.ru>
2022-10-14 22:46:32 +0300

    main добавлена с адаптивной версткой

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './Header';
+import Promo from './Main/Promo';
 
 function Main() {
     return (
       <main className="page">
         <Header link="/signin" />
+        <Promo />
       </main>
     );
   }
